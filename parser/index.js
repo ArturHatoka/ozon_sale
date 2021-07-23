782136bdc0844eb0cf32ed99ef15c2fa9eb66ed5
khasanshinae <khasanshinae@oooinex.ru>
2021-07-23 11:07:06 +0300

    no message

--- parser/index.js
+++ parser/index.js
@@ -1,24 +1,38 @@
 const {Builder, By, Key, until, Capabilities} = require('selenium-webdriver');
 // var webdriver = require('selenium-webdriver');
-var chrome = require('selenium-webdriver/chrome');
-var path = require('chromedriver').path;
+const chrome = require('selenium-webdriver/chrome');
+const path = require('chromedriver').path;
 
-var service = new chrome.ServiceBuilder(path).build();
+const service = new chrome.ServiceBuilder(path).build();
 chrome.setDefaultService(service);
 
-// var driver = new webdriver.Builder()
-//     .withCapabilities(webdriver.Capabilities.chrome())
-//     .build();
+const screen = {
+  width: 1280,
+  height: 720
+};
 
 async function example() {
   let driver = await new Builder()
+    .forBrowser('chrome')
+    .setChromeOptions(new chrome.Options().headless().windowSize(screen))
     .withCapabilities(Capabilities.chrome())
-    // .forBrowser('chrome')
     .build();
+
+  let log = (element)=>{
+     element.getText().then(function(text){
+            console.log(text);
+        });
+  }
+
   try {
-    await driver.get('http://www.google.com/ncr');
+    await driver.get('https://www.ozon.ru');
     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    // let elem = await driver.findElement(By.css('[data-widget="loginButton"]'))
+    // let elem = await driver.findElement(By.css('[title="Вход или регистрация"]'))
+    // log(elem)
+    // elem.click()
+    // console.log('success')
+    await driver.wait(until.titleIs('webdriver - Google Search'), 100000);
   } finally {
     await driver.quit();
   }
